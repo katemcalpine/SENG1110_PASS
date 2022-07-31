735a1ee8688b6eff848d778eabdd8ab874430a10
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2022-08-01 00:31:25 +1000

    Create S2W3_Q4.java

---
+++ S2W3_Q4.java
@@ -0,0 +1,29 @@
+//--------------------------------------------------------------------
+// Title: S2W3_Q4.java
+// Author: Kate McAlpine
+// Email: kate.mcalpine@newcastle.edu.au
+// Created: 6th March, 2022
+// Attached to worksheet: 'Java Basics' (Session 2, Week 3)
+//--------------------------------------------------------------------
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    long tBone;
+    char grilled = 'O';
+    tBone = (long)grilled;
+    System.out.println((char)tBone);
+  }
+}
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    float aBoat = 1.5;
+    byte anApple;
+    anApple = (byte)aBoat;
+    System.out.println((float)anApple);
+  }
+}
