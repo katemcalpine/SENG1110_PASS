406331fe5b13f429c1e1f15a8bb050ccae605ab4
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2022-08-01 00:15:08 +1000

    Create S2W3_Q6.java

---
+++ S2W3_Q6.java
@@ -0,0 +1,24 @@
+//--------------------------------------------------------------------
+// Title: S2W3_Q6.java
+// Author: Kate McAlpine
+// Email: kate.mcalpine@newcastle.edu.au
+// Created: 6th March, 2022
+// Attached to worksheet: 'Admin and Getting Started with Java' (Session 1, Week 2)
+//--------------------------------------------------------------------
+
+import java.util.Scanner;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Scanner keyboard = new Scanner(System.in);
+    double hours;                                 // Will be initialised when hours are input.
+    final double PAY_RATE = 1000.00;              // Initialising a constant.
+    double total;                                 // Will be initialised after calculation.
+    System.out.println("Please enter the hours you worked: ");
+    hours = keyboard.nextDouble();                // Only accepts an input that fits into a double.
+    total = hours * PAY_RATE;
+    System.out.printf("Total pay you will receive is: $%.2f", total); // Very C syntax.
+  }
+}
