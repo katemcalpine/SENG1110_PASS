cc761ff55a7dc1a8d09c5424577ee3cc5dc6aa92
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2022-08-22 22:03:54 +1000

    Create S5W6_Q1a.java

---
+++ S5W6_Q1a.java
@@ -0,0 +1,21 @@
+//--------------------------------------------------------------------
+// Title: S5W6_Q1a.java
+// Author: Kate McAlpine
+// Email: kate.mcalpine@newcastle.edu.au
+// Created: 29th March, 2022
+// Attached to worksheet: 'Classes and Methods 1' (Session 5, Week 4)
+//--------------------------------------------------------------------
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+        int x = 0;
+        for (int i = 0; i < 10; i++) {
+        	int temp = x;
+        	temp++;
+        	x = temp;
+        }
+        System.out.println(x);
+    }
+}
