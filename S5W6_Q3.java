d77ef8b88055121c36add339cbbbe3c15a18226e
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2022-08-22 22:43:53 +1000

    Create S5W6_Q3.java

---
+++ S5W6_Q3.java
@@ -0,0 +1,7 @@
+//--------------------------------------------------------------------
+// Title: S5W6_Q3.java
+// Author: Kate McAlpine
+// Email: kate.mcalpine@newcastle.edu.au
+// Created: 29th March, 2022
+// Attached to worksheet: 'Classes and Methods 1' (Session 5, Week 4)
+//--------------------------------------------------------------------
