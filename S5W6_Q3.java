92e8df3c4b2633cea22cfdf2a3c17c45b72a2d0c
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2022-08-25 20:11:51 +1000

    Update S5W6_Q3.java

--- S5W6_Q3.java
+++ S5W6_Q3.java
@@ -5,3 +5,22 @@
 // Created: 29th March, 2022
 // Attached to worksheet: 'Classes and Methods 1' (Session 5, Week 6)
 //--------------------------------------------------------------------
+
+import java.util.*;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.print("Please enter in a words that is 3 letters or longer, and has an ofdd amount of letters: ");
+    String strWord = keyboard.next();
+    while (strWord.length() < 3)
+    {
+      System.out.print("Please enter in a words that is 3 letters or longer, and has an ofdd amount of letters: ");
+      strWord = keyboard.next();
+    }
+    char[] charAWord = strWord.toCharArray();
+    System.out.print("Middle 3 letters are: " + charAword[(strWord.length()/2)-1] + charAWord[strWord.length()/2] + charAWord[strWord.length()/2)+1]);
+  }
+}
