c2fbc1eb96f7a2dc42601084f944fdc1dfd0333c
katemcalpine <49174249+katemcalpine@users.noreply.github.com>
2022-08-25 19:51:31 +1000

    Update S5W6_Q2a.java

--- S5W6_Q2a.java
+++ S5W6_Q2a.java
@@ -5,3 +5,28 @@
 // Created: 29th March, 2022
 // Attached to worksheet: 'Classes and Methods 1' (Session 5, Week 6)
 //--------------------------------------------------------------------
+
+public class Flowers
+{
+  private String name;
+  
+  public void setName(String flower)
+  {
+    name = flower;
+  }
+  
+  public String getName()
+  {
+    return name;
+  }
+}
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Flowers flos = new Flowers();
+    flos.setName("Daisy");
+    System.outprintln("Flower name is: " + flos.getName());
+  }
+}
